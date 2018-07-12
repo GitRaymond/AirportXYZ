4e3bc003b9651b1f7b70000644b5d77458e826ea
Wensveen <raymond@brackets.ninja>
2018-07-12 09:45:38 +0200

    added domains

---
+++ src/main/java/nl/raymond/airlinexyz/airlinexyz/domain/Airport.java
@@ -0,0 +1,34 @@
+package nl.raymond.airlinexyz.airlinexyz.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "airport")
+    private Set<Fleet> fleet = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/nl/raymond/airlinexyz/airlinexyz/domain/Fleet.java
@@ -0,0 +1,57 @@
+package nl.raymond.airlinexyz.airlinexyz.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+public class Fleet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String airplane;
+
+    private Integer fuel;
+
+    @ManyToOne
+    @JsonIgnoreProperties("fleets")
+    private Airport airport;
+
+
+    // METHODS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAirplane() {
+        return airplane;
+    }
+
+    public void setAirplane(String airplane) {
+        this.airplane = airplane;
+    }
+
+    public Integer getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(Integer fuel) {
+        this.fuel = fuel;
+    }
+
+    public Airport getAirport() {
+        return airport;
+    }
+
+    public void setAirport(Airport airport) {
+        this.airport = airport;
+    }
+}
