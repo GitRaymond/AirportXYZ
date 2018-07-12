a1cc22838d8c02a64fe3b141e397b2e33ac62916
Wensveen <raymond@brackets.ninja>
2018-07-12 09:27:44 +0200

    initial commit

---
+++ src/main/java/nl/raymond/airlinexyz/airlinexyz/AirlinexyzApplication.java
@@ -0,0 +1,12 @@
+package nl.raymond.airlinexyz.airlinexyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinexyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinexyzApplication.class, args);
+	}
+}
